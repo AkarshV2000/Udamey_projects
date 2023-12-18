dc2a2d9fd8de6d9def2f6ba12ae3d1b103aad0ef
unknown <vermaakarsh2000@gmail.com>
2023-12-18 20:56:22 +0530

    Day 4 exercise

---
+++ Udamey_day4.py
@@ -0,0 +1,10 @@
+#randomizaton in python
+import random #imprt the random module
+
+#program for head and tails
+
+res = random.randint(0,1)
+if res == 0: print("Tails")
+if res == 1: print("Heads")
+
+# there are many different functions present in import module, make sure to check them out
