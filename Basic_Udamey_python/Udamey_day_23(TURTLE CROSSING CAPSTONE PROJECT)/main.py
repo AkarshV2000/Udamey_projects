b1924809514f5f4e3d761525d942a386186537aa
AkarshV2000 <vermaakarsh2000@gmail.com>
2024-09-25 21:37:39 +0530

    New Turtle Crossing Project

--- Basic_Udamey_python/Udamey_day_23(TURTLE CROSSING CAPSTONE PROJECT)/main.py
+++ Basic_Udamey_python/Udamey_day_23(TURTLE CROSSING CAPSTONE PROJECT)/main.py
@@ -1,13 +1,16 @@
 from turtle import Turtle, Screen
 import time
 from player import Player
-
+from car_manager import CarManager
+from scorecard import Scoreboard
 
 screen = Screen()
 screen.setup(width=600, height=600)
 screen.tracer(0)
 
 player = Player()
+car_manager = CarManager()
+scoreboard = Scoreboard()
 
 screen.listen()
 screen.onkey(player.go_up, "Up")
@@ -16,6 +19,21 @@ game_is_on = True
 while game_is_on:
     time.sleep(0.1)
     screen.update()
+    car_manager.create_car()
+    car_manager.move_car()
+    
+
+    # Detect collision with the car
+    for car in car_manager.all_cars:
+        if car.distance(player) < 20:
+            game_is_on = False
+            scoreboard.game_over()
+
+    #Detect a successfull crossing
+    if player.is_at_finish_line():
+        player.goto_start()
+        car_manager.level_up()
+        scoreboard.increase_level()
 
     # if player.ycor()>=280:
     #     game_is_on = False
--- Basic_Udamey_python/Udamey_day_23(TURTLE CROSSING CAPSTONE PROJECT)/player.py
+++ Basic_Udamey_python/Udamey_day_23(TURTLE CROSSING CAPSTONE PROJECT)/player.py
@@ -10,8 +10,18 @@ class Player(Turtle):
         super().__init__()
         self.shape("turtle")
         self.penup()
-        self.goto(POSITION)
+        self.goto_start()
         self.setheading(90)
     
     def go_up(self):
         self.forward(MOVE_DISTANCE)
+
+    def is_at_finish_line(self):
+        if self.ycor() > FINISH_LINE:
+            return True
+        else:
+            return False
+        
+    def goto_start(self):
+        self.goto(POSITION)
+
